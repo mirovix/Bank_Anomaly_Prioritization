b03d0385c7db1ef32547cccaedd9a3505ab3a777
miro <miroljubmihailovic98@gmail.com>
2022-11-03 12:48:06 +0100

    refactor: removing extra queries

--- querys_DB/Account_ML.sql
+++
@@ -1,7 +0,0 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT DISTINCT [mm_dwa].[dbo].[ACCOUNT].[CODE_ACCOUNT]
-      ,[mm_dwa].[dbo].[ACCOUNT].[START_DATE]
-      ,[mm_dwa].[dbo].[ACCOUNT].[EXPIRE_DATE]
-	  ,[mm_dwa].[dbo].[ACCOUNT_SUBJECT].[NDG]
-  FROM [mm_dwa].[dbo].[ACCOUNT]
-  INNER JOIN  [mm_dwa].[dbo].[ACCOUNT_SUBJECT] ON [mm_dwa].[dbo].[ACCOUNT].[CODE_ACCOUNT] =  [mm_dwa].[dbo].[ACCOUNT_SUBJECT].[CODE_ACCOUNT]
--- querys_DB/Num_Residence_prov_ML.sql
+++
@@ -1,6 +0,0 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT [RESIDENCE_PROVINCE], count(*) as n
-
- FROM [mm_dwa].[dbo].[SUBJECT]
- group by [RESIDENCE_PROVINCE]
- order  by n desc
